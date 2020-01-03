ee33474d6ceded8c674e499bf130193847afc113
Curtion <curtion@126.com>
2020-01-03 15:20:57 +0000

    add num.461

---
+++ bit-manipulation/461-汉明距离-easy.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=461 lang=javascript
+ *
+ * [461] 汉明距离
+ *
+ * https://leetcode-cn.com/problems/hamming-distance/description/
+ *
+ * algorithms
+ * Easy (73.37%)
+ * Likes:    222
+ * Dislikes: 0
+ * Total Accepted:    37.2K
+ * Total Submissions: 50.3K
+ * Testcase Example:  '1\n4'
+ *
+ * 两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+ * 
+ * 给出两个整数 x 和 y，计算它们之间的汉明距离。
+ * 
+ * 注意：
+ * 0 ≤ x, y < 2^31.
+ * 
+ * 示例:
+ * 
+ * 
+ * 输入: x = 1, y = 4
+ * 
+ * 输出: 2
+ * 
+ * 解释:
+ * 1   (0 0 0 1)
+ * 4   (0 1 0 0)
+ * ⁠      ↑   ↑
+ * 
+ * 上面的箭头指出了对应二进制位不同的位置。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function (x, y) {
+  let num = x ^ y
+  let res = 0
+  for (let item of num.toString(2)) {
+    if (item === '1') {
+      res = res + 1
+    }
+  }
+  return res
+};
+// @lc code=end
+
