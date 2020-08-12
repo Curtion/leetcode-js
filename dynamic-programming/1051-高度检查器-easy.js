c6684bcc8ddb315386ee271d22b58e2142575e9b
Curtion <curtion@126.com>
2020-08-12 10:19:19 +0800

    1051-高度检测器

---
+++ dynamic-programming/1051-高度检查器-easy.js
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=1051 lang=javascript
+ *
+ * [1051] 高度检查器
+ *
+ * https://leetcode-cn.com/problems/height-checker/description/
+ *
+ * algorithms
+ * Easy (73.39%)
+ * Likes:    62
+ * Dislikes: 0
+ * Total Accepted:    22.2K
+ * Total Submissions: 30.2K
+ * Testcase Example:  '[1,1,4,2,1,3]'
+ *
+ * 学校在拍年度纪念照时，一般要求学生按照 非递减 的高度顺序排列。
+ * 
+ * 请你返回能让所有学生以 非递减 高度排列的最小必要移动人数。
+ * 
+ * 注意，当一组学生被选中时，他们之间可以以任何可能的方式重新排序，而未被选中的学生应该保持不动。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：heights = [1,1,4,2,1,3]
+ * 输出：3 
+ * 解释：
+ * 当前数组：[1,1,4,2,1,3]
+ * 目标数组：[1,1,1,2,3,4]
+ * 在下标 2 处（从 0 开始计数）出现 4 vs 1 ，所以我们必须移动这名学生。
+ * 在下标 4 处（从 0 开始计数）出现 1 vs 3 ，所以我们必须移动这名学生。
+ * 在下标 5 处（从 0 开始计数）出现 3 vs 4 ，所以我们必须移动这名学生。
+ * 
+ * 示例 2：
+ * 
+ * 输入：heights = [5,1,2,3,4]
+ * 输出：5
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：heights = [1,2,3,4,5]
+ * 输出：0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= heights.length <= 100
+ * 1 <= heights[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function (heights) {
+  const split = (arr) => {
+    if (arr.length === 1) {
+      return arr
+    }
+    if (arr.length === 0) {
+      return []
+    }
+    let left = []
+    let right = []
+    let base = arr.splice(0, 1);
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > base[0]) {
+        right.push(arr[i])
+      } else {
+        left.push(arr[i])
+      }
+    }
+    return split(left).concat(base, split(right))
+  }
+  let res = split([...heights])
+  let num = 0
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] !== heights[i]) {
+      num++
+    }
+  }
+  return num
+};
+// @lc code=end
+
