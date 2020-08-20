ca8abe4f027231ed3ebe46724933a407e5caabdb
Curtion <curtion@126.com>
2020-08-20 15:26:45 +0800

    141-环形链表

---
+++ linked-list/141-环形链表-easy.js
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ *
+ * https://leetcode-cn.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (49.07%)
+ * Likes:    721
+ * Dislikes: 0
+ * Total Accepted:    211.4K
+ * Total Submissions: 430.8K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表，判断链表中是否有环。
+ * 
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  if (head === null) {
+    return false
+  }
+  let arr = []
+  let flag = false
+  let list = (head) => {
+    if (head.next === null) {
+      return
+    }
+    arr.push(head.next)
+    if (arr.length !== new Set(arr).size) {
+      flag = true
+      return
+    }
+    list(head.next)
+  }
+  list(head)
+  return flag
+};
+// @lc code=end
+
