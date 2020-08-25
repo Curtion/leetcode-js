3e678eabdad3255f0ba1acc610bbe9a1b16571a1
Curtion <curtion@126.com>
2020-08-25 14:08:05 +0800

    226-翻转二叉树

---
+++ tree/226-翻转二叉树-easy.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=javascript
+ *
+ * [226] 翻转二叉树
+ *
+ * https://leetcode-cn.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (76.27%)
+ * Likes:    550
+ * Dislikes: 0
+ * Total Accepted:    110K
+ * Total Submissions: 144.1K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * 翻转一棵二叉树。
+ * 
+ * 示例：
+ * 
+ * 输入：
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 2     7
+ * ⁠/ \   / \
+ * 1   3 6   9
+ * 
+ * 输出：
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 7     2
+ * ⁠/ \   / \
+ * 9   6 3   1
+ * 
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ * 
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  let tree = (root) => {
+    if (root === null) {
+      return root
+    }
+    let left = tree(root.left)
+    let right = tree(root.right)
+    let newTree = new TreeNode(root.val)
+    newTree.left = right
+    newTree.right = left
+    return newTree
+  }
+  return tree(root)
+};
+// @lc code=end
+
