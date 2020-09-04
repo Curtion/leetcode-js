e43d0366b1ef5716608acdf555fcc194c217ea1e
Curtion <curtion@126.com>
2020-09-04 11:44:48 +0800

    231-2的幂

---
+++ math/231-2-的幂-easy.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=231 lang=javascript
+ *
+ * [231] 2的幂
+ *
+ * https://leetcode-cn.com/problems/power-of-two/description/
+ *
+ * algorithms
+ * Easy (48.58%)
+ * Likes:    238
+ * Dislikes: 0
+ * Total Accepted:    76.9K
+ * Total Submissions: 158.3K
+ * Testcase Example:  '1'
+ *
+ * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1
+ * 输出: true
+ * 解释: 2^0 = 1
+ * 
+ * 示例 2:
+ * 
+ * 输入: 16
+ * 输出: true
+ * 解释: 2^4 = 16
+ * 
+ * 示例 3:
+ * 
+ * 输入: 218
+ * 输出: false
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+  // if (n === 0) return false
+  // while (n !== 1) {
+  //   if (n % 2 !== 0) {
+  //     return false
+  //   }
+  //   n = n / 2
+  // }
+  // return true
+  return n > 0 && (n & (n - 1)) === 0
+};
+// @lc code=end
+
