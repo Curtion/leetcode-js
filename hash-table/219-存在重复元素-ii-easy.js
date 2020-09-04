1e1ba738ed9ac8ec61ba9487f34c66ade66c90cc
Curtion <curtion@126.com>
2020-09-04 11:19:18 +0800

    219-存在重复元素-ii

---
+++ hash-table/219-存在重复元素-ii-easy.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=javascript
+ *
+ * [219] 存在重复元素 II
+ *
+ * https://leetcode-cn.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (40.08%)
+ * Likes:    193
+ * Dislikes: 0
+ * Total Accepted:    58.6K
+ * Total Submissions: 145.5K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的
+ * 绝对值 至多为 k。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums = [1,2,3,1], k = 3
+ * 输出: true
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums = [1,0,1,1], k = 1
+ * 输出: true
+ * 
+ * 示例 3:
+ * 
+ * 输入: nums = [1,2,3,1,2,3], k = 2
+ * 输出: false
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j <= i + k; j++) {
+      if (nums[i] === nums[j] && j - i <= k) {
+        return true
+      }
+    }
+  }
+  return false
+};
+// @lc code=end
+
