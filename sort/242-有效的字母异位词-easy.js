992501c622460e794513c6e776ac406e7df174a0
Curtion <curtion@126.com>
2020-09-25 16:13:58 +0800

    242-有效的字母异味词

---
+++ sort/242-有效的字母异位词-easy.js
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=javascript
+ *
+ * [242] 有效的字母异位词
+ *
+ * https://leetcode-cn.com/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (61.25%)
+ * Likes:    254
+ * Dislikes: 0
+ * Total Accepted:    140.6K
+ * Total Submissions: 228.4K
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * 
+ * 示例 1:
+ * 
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ * 
+ * 说明:
+ * 你可以假设字符串只包含小写字母。
+ * 
+ * 进阶:
+ * 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  let sArr = s.split('')
+  let tArr = t.split('')
+  for (let i = 0; i < sArr.length; i++) {
+    if (tArr.includes(sArr[i])) {
+      tArr.splice(tArr.indexOf(sArr[i]), 1)
+    } else {
+      return false
+    }
+  }
+  return tArr.length === 0
+};
+// @lc code=end
+
